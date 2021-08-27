4a110c20ead397f4a83d2810d87705275941076b
yfirmansyah <89082073+yfirmansyah@users.noreply.github.com>
2021-08-27 21:56:08 +0700

    Update update.php

--- update.php
+++ update.php
@@ -7,8 +7,12 @@
     $id     = $_POST['id'];
     $npm    = $_POST['npm'];
     $nama   = $_POST['nama'];
-    
     mysqli_query($con,"UPDATE mahasiswa set npm='$npm', nama='$nama' where id='$id'");
     header("location:latihan1.php");
 
+    $id_dosen    = $_POST['id_dosen'];
+    $kode_dosen  = $_POST['kode_dosen'];
+    $nama_dosen  = $_POST['nama_dosen'];
+    mysqli_query($con,"UPDATE dosen set kode_dosen='$kode_dosen', nama_dosen='$nama_dosen' where id_dosen='$id_dosen'");
+    header("location:latihan5.php");
 ?>
